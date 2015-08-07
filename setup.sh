7f570729298779dd43f067ff071ea3ae9f15cf77
Erlend Røsjø <erlend@erlend.net>
2015-08-07 09:14:46 +0200

    Added more brew software

--- setup.sh
+++ setup.sh
@@ -16,6 +16,10 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
+#Update
+brew update
+brew upgrade brew-cask
+
 #Install software
 brew cask install google-chrome
 brew cask install sublime-text3
@@ -23,7 +27,15 @@ brew cask install atom
 brew cask install spotify
 brew cask install iterm2
 brew cask install sizeup
+brew cask install virtualbox
+brew cask install hipchat
 
 #Install CLI software
 brew install git
 brew install node
+brew install go
+brew install elixir
+brew install python
+brew install wget
+brew install boot2docker
+brew install docker
